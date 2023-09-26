5285e46ab07f21feb593f842fbe878c41e1649e3
tejavathlal <tejavathlal@gmail.com>
2023-09-26 12:29:01 +0530

    login

--- app.module.ts
+++ app.module.ts
@@ -7,6 +7,12 @@ import { LoginComponent } from './component/login/login.component';
 import { DashboardComponent } from './component/dashboard/dashboard.component';
 import { CreatestudentComponent } from './component/createstudent/createstudent.component';
 import { StudenttableformComponent } from './component/studenttableform/studenttableform.component';
+import { PagenotfoundComponent } from './component/pagenotfound/pagenotfound.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -14,11 +20,17 @@ import { StudenttableformComponent } from './component/studenttableform/studentt
     LoginComponent,
     DashboardComponent,
     CreatestudentComponent,
-    StudenttableformComponent
+    StudenttableformComponent,
+    PagenotfoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
